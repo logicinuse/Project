df5a8b6eb84e173108db378524f20800b57caf68
logicinuse <33352761+logicinuse@users.noreply.github.com>
2018-10-09 13:16:59 -0500

    Add files via upload

---
+++ frmLetter.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace AACSPROJECT_AD
+{
+    partial class frmLetter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUcase = new System.Windows.Forms.Button();
+            this.btnLcase = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnTrim = new System.Windows.Forms.Button();
+            this.btnStatistics = new System.Windows.Forms.Button();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblOrg = new System.Windows.Forms.Label();
+            this.lblDept = new System.Windows.Forms.Label();
+            this.lblCourses = new System.Windows.Forms.Label();
+            this.lblCareer = new System.Windows.Forms.Label();
+            this.lblResources = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblSender = new System.Windows.Forms.Label();
+            this.txtOrg = new System.Windows.Forms.TextBox();
+            this.txtDept = new System.Windows.Forms.TextBox();
+            this.txtCourses = new System.Windows.Forms.TextBox();
+            this.txtCareer = new System.Windows.Forms.TextBox();
+            this.txtCost = new System.Windows.Forms.TextBox();
+            this.txtResources = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblCost = new System.Windows.Forms.Label();
+            this.txtSender = new System.Windows.Forms.TextBox();
+            this.txtFormOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnUcase
+            // 
+            this.btnUcase.Location = new System.Drawing.Point(502, 565);
+            this.btnUcase.Name = "btnUcase";
+            this.btnUcase.Size = new System.Drawing.Size(134, 25);
+            this.btnUcase.TabIndex = 16;
+            this.btnUcase.Text = "UCase TCC";
+            this.btnUcase.UseVisualStyleBackColor = true;
+            this.btnUcase.Click += new System.EventHandler(this.btnUcase_Click);
+            // 
+            // btnLcase
+            // 
+            this.btnLcase.Location = new System.Drawing.Point(344, 565);
+            this.btnLcase.Name = "btnLcase";
+            this.btnLcase.Size = new System.Drawing.Size(136, 25);
+            this.btnLcase.TabIndex = 15;
+            this.btnLcase.Text = "LCase TCC";
+            this.btnLcase.UseVisualStyleBackColor = true;
+            this.btnLcase.Click += new System.EventHandler(this.btnLcase_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(301, 523);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnTrim
+            // 
+            this.btnTrim.Location = new System.Drawing.Point(405, 523);
+            this.btnTrim.Name = "btnTrim";
+            this.btnTrim.Size = new System.Drawing.Size(75, 23);
+            this.btnTrim.TabIndex = 12;
+            this.btnTrim.Text = "Trim";
+            this.btnTrim.UseVisualStyleBackColor = true;
+            this.btnTrim.Click += new System.EventHandler(this.btnTrim_Click);
+            // 
+            // btnStatistics
+            // 
+            this.btnStatistics.Location = new System.Drawing.Point(502, 523);
+            this.btnStatistics.Name = "btnStatistics";
+            this.btnStatistics.Size = new System.Drawing.Size(75, 23);
+            this.btnStatistics.TabIndex = 13;
+            this.btnStatistics.Text = "Statistics";
+            this.btnStatistics.UseVisualStyleBackColor = true;
+            this.btnStatistics.Click += new System.EventHandler(this.btnStatistics_Click);
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.Location = new System.Drawing.Point(602, 523);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(75, 23);
+            this.btnRestore.TabIndex = 14;
+            this.btnRestore.Text = "Restore";
+            this.btnRestore.UseVisualStyleBackColor = true;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.Location = new System.Drawing.Point(51, 17);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(100, 23);
+            this.lblFirstName.TabIndex = 0;
+            this.lblFirstName.Text = "First Name";
+            this.lblFirstName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.Location = new System.Drawing.Point(48, 83);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(100, 23);
+            this.lblLastName.TabIndex = 0;
+            this.lblLastName.Text = "Last Name";
+            this.lblLastName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblOrg
+            // 
+            this.lblOrg.Location = new System.Drawing.Point(48, 147);
+            this.lblOrg.Name = "lblOrg";
+            this.lblOrg.Size = new System.Drawing.Size(100, 23);
+            this.lblOrg.TabIndex = 0;
+            this.lblOrg.Text = "Organization";
+            this.lblOrg.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblDept
+            // 
+            this.lblDept.Location = new System.Drawing.Point(48, 209);
+            this.lblDept.Name = "lblDept";
+            this.lblDept.Size = new System.Drawing.Size(100, 23);
+            this.lblDept.TabIndex = 0;
+            this.lblDept.Text = "Department";
+            this.lblDept.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblCourses
+            // 
+            this.lblCourses.Location = new System.Drawing.Point(48, 268);
+            this.lblCourses.Name = "lblCourses";
+            this.lblCourses.Size = new System.Drawing.Size(100, 23);
+            this.lblCourses.TabIndex = 0;
+            this.lblCourses.Text = "Courses";
+            this.lblCourses.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblCareer
+            // 
+            this.lblCareer.Location = new System.Drawing.Point(48, 327);
+            this.lblCareer.Name = "lblCareer";
+            this.lblCareer.Size = new System.Drawing.Size(100, 23);
+            this.lblCareer.TabIndex = 0;
+            this.lblCareer.Text = "Career";
+            this.lblCareer.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblResources
+            // 
+            this.lblResources.Location = new System.Drawing.Point(51, 446);
+            this.lblResources.Name = "lblResources";
+            this.lblResources.Size = new System.Drawing.Size(100, 23);
+            this.lblResources.TabIndex = 0;
+            this.lblResources.Text = "Resources";
+            this.lblResources.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.Location = new System.Drawing.Point(51, 499);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(100, 23);
+            this.lblPhone.TabIndex = 0;
+            this.lblPhone.Text = "Phone";
+            this.lblPhone.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblSender
+            // 
+            this.lblSender.Location = new System.Drawing.Point(51, 557);
+            this.lblSender.Name = "lblSender";
+            this.lblSender.Size = new System.Drawing.Size(100, 23);
+            this.lblSender.TabIndex = 0;
+            this.lblSender.Text = "Sender";
+            this.lblSender.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtOrg
+            // 
+            this.txtOrg.Location = new System.Drawing.Point(51, 173);
+            this.txtOrg.Name = "txtOrg";
+            this.txtOrg.Size = new System.Drawing.Size(182, 20);
+            this.txtOrg.TabIndex = 3;
+            // 
+            // txtDept
+            // 
+            this.txtDept.Location = new System.Drawing.Point(51, 235);
+            this.txtDept.Name = "txtDept";
+            this.txtDept.Size = new System.Drawing.Size(182, 20);
+            this.txtDept.TabIndex = 4;
+            // 
+            // txtCourses
+            // 
+            this.txtCourses.Location = new System.Drawing.Point(51, 294);
+            this.txtCourses.Name = "txtCourses";
+            this.txtCourses.Size = new System.Drawing.Size(182, 20);
+            this.txtCourses.TabIndex = 5;
+            // 
+            // txtCareer
+            // 
+            this.txtCareer.Location = new System.Drawing.Point(51, 353);
+            this.txtCareer.Name = "txtCareer";
+            this.txtCareer.Size = new System.Drawing.Size(182, 20);
+            this.txtCareer.TabIndex = 6;
+            // 
+            // txtCost
+            // 
+            this.txtCost.Location = new System.Drawing.Point(51, 411);
+            this.txtCost.Name = "txtCost";
+            this.txtCost.Size = new System.Drawing.Size(182, 20);
+            this.txtCost.TabIndex = 7;
+            // 
+            // txtResources
+            // 
+            this.txtResources.Location = new System.Drawing.Point(51, 472);
+            this.txtResources.Name = "txtResources";
+            this.txtResources.Size = new System.Drawing.Size(182, 20);
+            this.txtResources.TabIndex = 8;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(51, 525);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(182, 20);
+            this.txtPhone.TabIndex = 9;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(54, 43);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(182, 20);
+            this.txtFirstName.TabIndex = 1;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(54, 109);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(179, 20);
+            this.txtLastName.TabIndex = 2;
+            // 
+            // lblCost
+            // 
+            this.lblCost.Location = new System.Drawing.Point(51, 385);
+            this.lblCost.Name = "lblCost";
+            this.lblCost.Size = new System.Drawing.Size(100, 23);
+            this.lblCost.TabIndex = 0;
+            this.lblCost.Text = "Cost";
+            this.lblCost.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtSender
+            // 
+            this.txtSender.Location = new System.Drawing.Point(54, 583);
+            this.txtSender.Name = "txtSender";
+            this.txtSender.Size = new System.Drawing.Size(179, 20);
+            this.txtSender.TabIndex = 10;
+            // 
+            // txtFormOutput
+            // 
+            this.txtFormOutput.Location = new System.Drawing.Point(301, 43);
+            this.txtFormOutput.Multiline = true;
+            this.txtFormOutput.Name = "txtFormOutput";
+            this.txtFormOutput.Size = new System.Drawing.Size(376, 375);
+            this.txtFormOutput.TabIndex = 17;
+            // 
+            // frmLetter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 623);
+            this.Controls.Add(this.txtFormOutput);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtSender);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtResources);
+            this.Controls.Add(this.txtCost);
+            this.Controls.Add(this.txtCareer);
+            this.Controls.Add(this.txtCourses);
+            this.Controls.Add(this.txtDept);
+            this.Controls.Add(this.txtOrg);
+            this.Controls.Add(this.lblSender);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblResources);
+            this.Controls.Add(this.lblCareer);
+            this.Controls.Add(this.lblCourses);
+            this.Controls.Add(this.lblDept);
+            this.Controls.Add(this.lblOrg);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblCost);
+            this.Controls.Add(this.btnRestore);
+            this.Controls.Add(this.btnStatistics);
+            this.Controls.Add(this.btnTrim);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnLcase);
+            this.Controls.Add(this.btnUcase);
+            this.Name = "frmLetter";
+            this.Text = "Form Letter";
+            this.Load += new System.EventHandler(this.frmLetter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnUcase;
+        private System.Windows.Forms.Button btnLcase;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnTrim;
+        private System.Windows.Forms.Button btnStatistics;
+        private System.Windows.Forms.Button btnRestore;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblOrg;
+        private System.Windows.Forms.Label lblDept;
+        private System.Windows.Forms.Label lblCourses;
+        private System.Windows.Forms.Label lblCareer;
+        private System.Windows.Forms.Label lblResources;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblSender;
+        private System.Windows.Forms.TextBox txtOrg;
+        private System.Windows.Forms.TextBox txtDept;
+        private System.Windows.Forms.TextBox txtCourses;
+        private System.Windows.Forms.TextBox txtCareer;
+        private System.Windows.Forms.TextBox txtCost;
+        private System.Windows.Forms.TextBox txtResources;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblCost;
+        private System.Windows.Forms.TextBox txtSender;
+        private System.Windows.Forms.TextBox txtFormOutput;
+    }
+}
---
+++ frmLetter.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AACSPROJECT_AD
+{
+    public partial class frmLetter : Form
+    {
+        public frmLetter()
+        {
+            InitializeComponent();
+        }
+        string formLetter = "";
+
+        private void frmLetter_Load(object sender, EventArgs e)
+        {
+            txtFormOutput.Text = "\r\n\r\n\r\nTulsa Community   College" +
+                "\r\n\r\n\r\nDear {0}{1}" + 
+                "\r\n\r\n\r\nWe here at {2} want to let you know about a keen new program operated by the {3} department.  " +
+                "Now for a limited time, you can take courses in {4}. which can lead to an exciting career in {5}, " +
+                "which is going to be a growth industry in the coming years.  The cost for thid experience is a mere {6}." +
+                "\r\n\r\n\r\nSign up today to get more {7} in this fantastic opportunity.  Call us at {8} to request an information bundle today" +
+                "\r\n\r\n\r\nYours very truly," +
+                "\r\n\r\n\r\n{9}";
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            txtFormOutput.Text = txtFormOutput.Text.Replace("{0}", txtFirstName.Text + " ")
+            .Replace("{1}", txtLastName.Text + ",")
+            .Replace("{2}", txtOrg.Text)
+            .Replace("{3}", txtDept.Text)
+            .Replace("{4}", txtCourses.Text)
+            .Replace("{5}", txtCareer.Text)
+            .Replace("{6}", txtCost.Text)
+            .Replace("{7}", txtResources.Text)
+            .Replace("{8}", txtPhone.Text)
+            .Replace("{9}", txtSender.Text);
+        }
+
+        private void btnTrim_Click(object sender, EventArgs e)
+        {
+            formLetter = txtFormOutput.Text;
+            int loc = formLetter.IndexOf("  ");
+            if (loc > 0)
+            {
+                int newLine = 0;
+                string sub = String.Empty;
+                string secondSub = String.Empty;
+
+                newLine = formLetter.IndexOf("\r\n", loc);
+                int lengthForSubstring = newLine - loc;
+                sub = formLetter.Substring(loc, lengthForSubstring);
+                secondSub = sub.Trim();
+                formLetter = formLetter.Replace(sub, " " + secondSub);
+
+                txtFormOutput.Text = formLetter;
+            }
+        }
+
+        private void btnStatistics_Click(object sender, EventArgs e)
+        {
+            int iNoLines = 0;
+            int iLength = 0;
+
+            string formLetter = txtFormOutput.Text;
+
+            iLength = formLetter.Length;
+            iNoLines = System.Text.RegularExpressions.Regex.Matches(formLetter, "\n").Count;
+            txtFormOutput.Text = (formLetter + "\r\n\r\nThe number of characters in the letter is " + iLength + "." + 
+                "\r\n\r\nThe number of line feeds in the letter is " + iNoLines + ".");
+        }
+
+        private void btnRestore_Click(object sender, EventArgs e)
+        {
+            txtFormOutput.Text = "\r\n\r\n\r\nTulsa Community   College" +
+               "\r\n\r\n\r\nDear {0}{1}" +
+               "\r\n\r\n\r\nWe here at {2} want to let you know about a keen new program operated by the {3} department.  " +
+               "Now for a limited time, you can take courses in {4}. which can lead to an exciting career in {5}, " +
+               "which is going to be a growth industry in the coming years.  The cost for thid experience is a mere {6}." +
+               "\r\n\r\n\r\nSign up today to get more {7} in this fantastic opportunity.  Call us at {8} to request an information bundle today" +
+               "\r\n\r\n\r\nYours very truly," +
+               "\r\n\r\n\r\n{9}";
+        }
+
+        private void btnLcase_Click(object sender, EventArgs e)
+        {
+            formLetter = txtFormOutput.Text;
+            if (formLetter.Contains("Tulsa Community   College"))
+            {
+                formLetter = txtFormOutput.Text;
+                int loc = formLetter.IndexOf("Tulsa Community   College");
+             
+                if (loc > 0)
+                {
+                    int newLine = 0;
+                    string sub = String.Empty;
+                    string secondSub = String.Empty;
+
+                    newLine = formLetter.IndexOf("\r\n", loc);
+                    int lengthForSubstring = newLine - loc;
+                    sub = formLetter.Substring(loc, lengthForSubstring);
+                    secondSub = sub.ToLower();
+                    if(secondSub.Contains("  "))
+                    {
+                        secondSub = secondSub.Replace("   ", " ");
+                    }
+                    formLetter = formLetter.Replace(sub, secondSub);
+
+                    txtFormOutput.Text = formLetter;
+                }
+            }
+            else if (formLetter.Contains("TULSA COMMUNITY COLLEGE"))
+            {
+                formLetter = txtFormOutput.Text;
+                int loc = formLetter.IndexOf("TULSA COMMUNITY COLLEGE");
+                if (loc > 0)
+                {
+                    int newLine = 0;
+                    string sub = String.Empty;
+                    string secondSub = String.Empty;
+
+                    newLine = formLetter.IndexOf("\r\n", loc);
+                    int lengthForSubstring = newLine - loc;
+                    sub = formLetter.Substring(loc, lengthForSubstring);
+                    secondSub = sub.ToLower();
+                    formLetter = formLetter.Replace(sub, secondSub);
+
+                    txtFormOutput.Text = formLetter;
+                }
+            }
+        }
+
+        private void btnUcase_Click(object sender, EventArgs e)
+        {
+            formLetter = txtFormOutput.Text;
+            if (formLetter.Contains("Tulsa Community   College"))
+            {
+                formLetter = txtFormOutput.Text;
+                int loc = formLetter.IndexOf("Tulsa Community   College");
+
+                if (loc > 0)
+                {
+                    int newLine = 0;
+                    string sub = String.Empty;
+                    string secondSub = String.Empty;
+
+                    newLine = formLetter.IndexOf("\r\n", loc);
+                    int lengthForSubstring = newLine - loc;
+                    sub = formLetter.Substring(loc, lengthForSubstring);
+                    secondSub = sub.ToUpper();
+                    if (secondSub.Contains("  "))
+                    {
+                        secondSub = secondSub.Replace("   ", " ");
+                    }
+                    formLetter = formLetter.Replace(sub, secondSub);
+
+                    txtFormOutput.Text = formLetter;
+                }
+            }
+
+            else if (formLetter.Contains("tulsa community college"))
+            {
+                formLetter = txtFormOutput.Text;
+                int loc = formLetter.IndexOf("tulsa community college");
+                if (loc > 0)
+                {
+                    int newLine = 0;
+                    string sub = String.Empty;
+                    string secondSub = String.Empty;
+
+                    newLine = formLetter.IndexOf("\r\n", loc);
+                    int lengthForSubstring = newLine - loc;
+                    sub = formLetter.Substring(loc, lengthForSubstring);
+                    secondSub = sub.ToUpper();
+                    formLetter = formLetter.Replace(sub, secondSub);
+
+                    txtFormOutput.Text = formLetter;
+                }
+            }
+
+            else if (formLetter.Contains("Tulsa Community College"))
+            {
+                formLetter = txtFormOutput.Text;
+                int loc = formLetter.IndexOf("Tulsa Community College");
+                if (loc > 0)
+                {
+                    int newLine = 0;
+                    string sub = String.Empty;
+                    string secondSub = String.Empty;
+
+                    newLine = formLetter.IndexOf("\r\n", loc);
+                    int lengthForSubstring = newLine - loc;
+                    sub = formLetter.Substring(loc, lengthForSubstring);
+                    secondSub = sub.ToUpper();
+                    formLetter = formLetter.Replace(sub, secondSub);
+
+                    txtFormOutput.Text = formLetter;
+                }
+            }
+        }
+    }
+}
